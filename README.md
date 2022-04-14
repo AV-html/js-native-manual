ea34bad72b8583fe39c55cec9b818de25a1a241e
Alexander Volkov <AV-html@yandex.ru>
2022-04-14 17:21:42 +0300

    1-2 variables + output|input

---
+++ README.md
@@ -0,0 +1,8 @@
+# Native JavaScript (basic level) #
+
+## Tasks
+
+1. [Variables](./js/1.variables.md)
+
+
+1. [Output & input](./js/1.variables.md)
---
+++ js/2-output-input.js
@@ -0,0 +1,58 @@
+// * ===== Вывод в консоль ===== * //
+// Плагин:
+// JavaScript Snippet Pack
+// akamud.vscode-javascript-snippet-pack
+// Mahmoud Ali
+
+let userMoney = 9900;
+let userName = "Pavel";
+
+console.log(userMoney);
+console.log(userName);
+
+// Склеивание строк:
+// 1 способ:
+console.log(userName + "has" + userMoney); // склеивание
+
+// 2 способ:
+let result = userName + "has" + userMoney;
+console.log(result);
+
+
+
+// Как проверить тип: (оператор typeof возвращает строку с названием типа данных)
+console.log(typeof key);
+console.log(typeof userMoney);
+console.log(typeof userName);
+console.log(typeof isUserAccess);
+console.log(typeof book); // Объект - известная ошибка JS, должен быть тип null
+
+
+// Альтернатива: всплывающие окошки
+// al
+// alert("text")
+
+// * ===== Ввод с клавиатуры ===== * //
+// ! pm
+// let userText = prompt('сообщение'); // Если нажать отмену, то будет null
+// console.log(userText);
+
+// ! 1. Если хотим запросить символ/слово/текст
+// let userMessage = prompt('сообщение');
+
+// ! 2. Если хотим запросить цифру/число:
+// let userNumber = Number(prompt('сообщение'));
+
+
+
+// ##### Задача ##### //
+// 1. Задача
+// Сделать рефакторинг кода
+
+// chiclo = 10
+// monster = "false";
+// user__text = 'Какой-то текст";
+// console.log(chiclo boolian user__text)
+
+
+

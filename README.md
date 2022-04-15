91c3ae184a22cbb2854b06661755e012a4979f96
Alexander Volkov <AV-html@yandex.ru>
2022-04-15 22:00:32 +0300

    add string

--- README.md
+++ README.md
@@ -7,3 +7,5 @@
 2. [Output & input](./js/2-output-input.md)
 
 3. [Number](./js/3.number.md)
+
+4. [String](./js/4.string.md)
---
+++ js/4-string.js
@@ -0,0 +1,36 @@
+// * ===== string ===== * //
+
+// * Esc-последовательности (экранирование)
+// \n - enter (newline)
+// \t - tab
+// \' - single quote '
+// \" - double quote "
+// \\ - backslash \
+
+// Более редкие:
+// \r	carriage return
+// \b	word boundary
+// \f	form feed
+console.log();
+
+
+// Узнать количество символов
+let text = 'This is text of the user';
+console.log(text.length); // 24 - символа в строке text
+
+
+// Функции не меняют саму строку
+// console.log(text.toUpperCase()); // Возвращает строку в верхнем регистре
+
+// console.log(text.toLowerCase()); // Возвращает строку в нижнем регистре
+
+
+let numStr = String(24); // преоразовать в тип String
+console.log(numStr); // '24'
+
+
+let userName = 'Pavel';
+// * 3 способа вывести в консоль
+console.log("Моё имя -", userName); // Параметры через запятую
+console.log('Моё имя - ' + userName); // Конкатенация (склеивание)
+console.log(`Моё имя - ${userName}`); // Шаблонные литералы
